cf0cb1f45021b73f358f9177f747ecff0ebeb559
Olaf Janssen <o.t.a.janssen@gmail.com>
2021-03-31 14:12:02 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# MERGait
+This package assists in reading and processing running gait related data, varying from raw (iPhone) activity and IMU data to footpod per-step data.
+
+## Overview
+
+## Installation
+MERGait is compatible with python v3.6+
+
+<!-- **You can install MERGait via pip:**
+```sh
+pip install mergait
+``` -->
+
+## Basic usage
+
+## Contributing to the project
+
+## Acknowledgements
+
+## Author
+Olaf Janssen
+
+## License
+MERGait is under the MIT license
---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+
+def requirements():
+    with open('requirements.txt', "r") as fh:
+        return [x for x in fh.read().split('\n') if x]
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='mergait',
+                 version='0.0.1',
+                 description='Read and process running gait related dataframes, ranging from raw (iPhone) activity and IMU data to footpod per-step data and music parameters.',
+                 long_description=long_description,
+                 long_description_content_type="text/markdown",
+                 packages=setuptools.find_packages(),
+                 author='Olaf T.A. Janssen',
+                 author_email='olaf.janssen@fontys.nl',
+                 keywords=['running', 'gait', 'mergait', 'imu',
+                           'symmetry', 'accelerometer', 'footpods', 'spotify'],
+                 classifiers=["Programming Language :: Python :: 3.6",
+                              "License :: OSI Approved :: MIT License",
+                              "Operating System :: OS Independent"],
+                 license='MIT',
+                 python_requires='>=3.6',
+                 zip_safe=False,
+                 install_requires=requirements(),
+                 include_package_data=True)
